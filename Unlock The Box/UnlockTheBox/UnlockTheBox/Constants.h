27f2b60e005d4ca5b451e6390e13bb4bd07277a0
Michael Cottrell <runksmania1011@gmail.com>
2019-01-10 04:11:43 -0800

    Constants still had an incorrect string for defaultPrizeFile, it is fixed now. Rebuilt installer with updated code. Repackaged the unpacking installer with visual c++ redist 2015 update 3.

--- Unlock The Box/UnlockTheBox/UnlockTheBox/Constants.h
+++ Unlock The Box/UnlockTheBox/UnlockTheBox/Constants.h
@@ -29,14 +29,14 @@ public:
     static string defaultPrizeFile()
     {
         return "Prize name,Value, Each line is a prize tier.\n"
-            "TV Pkg 1, 750, 0\n"
-            "TV Pkg 2, 500, 0\n"
-            "TV1, 250, 0, Roomba, 250, 0\n"
-            "Cash 150, 150, 0, Cash 150, 150, 0\n"
-            "Cash 125, 125, 0, Cash 125, 125, 0\n"
-            "Free Play 80, 80, 0, Free Play 80, 80, 0, Cash 80, 80, 0, Cash 80, 80, 0\n"
-            "Cash 25,0, Cash 25,0, Cash 25,0, Cash 25,0, Cash 25,0, Cash 25,0, Cash 25,0, Cash 25,0, Cash 25,0, Cash 25,0, Cash 25,0\n"
-            "Cash 15, 15, 0\n";
+            "TV Pkg 1,750,0\n"
+            "TV Pkg 2,500,0\n"
+            "TV1,250,0,Roomba,250,0\n"
+            "Cash 150,150,0,Cash 150,150,0\n"
+            "Cash 125,125,0,Cash 125,125,0\n"
+            "Free Play 80,80,0,Free Play 80,80,0,Cash 80,80,0,Cash 80,80,0\n"
+            "Cash 25,25,0,Cash 25,25,0,Cash 25,25,0,Cash 25,25,0,Cash 25,25,0,Cash 25,25,0,Cash 25,25,0,Cash 25,25,0,Cash 25,25,0,Cash 25,25,0,Cash 25,25,0\n"
+            "Cash 15,15,0\n";
     }
 
     static bool getWithReplacement()
