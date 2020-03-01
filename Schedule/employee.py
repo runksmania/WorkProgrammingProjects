374745d5c356e4953a48c9d4962058ef82c0361c
Michael Cottrell <runksmania1011@gmail.com>
2020-02-29 21:39:15 -0800

    Delete employee.py
    Scrapping Project.

--- Schedule/employee.py
+++
@@ -1,8 +0,0 @@
-class Employee:
-  
-  def __init__(self, first_name, last_name):
-    self.first_name = first_name
-    self.last_name = last_name
-    self.shift = ''
-    self.first_day_off = ''
-    self.second_day_off = ''
